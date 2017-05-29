4c8f7842e4f42848b4b7c85b86018a217fc33fa7
Thomas Hlina <thomas.hlina@tugraz.at>
2017-05-29 23:19:46 +0200

    revised "ArduinoStyle" example

--- examples/ArduinoStyle/app/application.cpp
+++ examples/ArduinoStyle/app/application.cpp
@@ -1,29 +1,44 @@
 /**
- * Arduino style does not work in sming
- * the example crashes with "Software Watchdog Reset"
+ * Arduino style does not work with sming. The example runs into a watchdog reset.
+ * WDT.alive() can be uses in the loop to prevent the watchdog reset. But only for
+ * about 3.2 seconds. system_soft_wdt_stop() disables the software watchdog but after
+ * about 6 seconds the hardware watchdog resets the system.
  *
  * @see: SmingTemplate for the right way
  *
  */
+
+#include <user_config.h>
 #include <SmingCore/SmingCore.h>
+
 
 #define LED_PIN 2 // LED, TXD1
 
-bool state = true;
+// forward declation
+void setup();
 
 void loop();
 
+
+// this will be called by the the framework after booting
 void init() {
-  pinMode(LED_PIN, OUTPUT);
-  loop();
+  WifiStation.enable(false);
+  setup();
+  while (true) {
+    loop();
+  }
 }
 
 
+// begin of arduino style of programming
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+}
+
 void loop() {
-  while (true) {
-    digitalWrite(LED_PIN, HIGH);
-    delay(1000);
-    digitalWrite(LED_PIN, LOW);
-    delay(1000);
-  }
+  digitalWrite(LED_PIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_PIN, LOW);
+  delay(1000);
+//  WDT.alive();
 }
