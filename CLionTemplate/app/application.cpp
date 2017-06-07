1a76d56419c050882757ca835f950eadf9919da7
Thomas Hlina <thomas.hlina@tugraz.at>
2017-06-07 10:27:22 +0200

    fixed compile error

--- CLionTemplate/app/application.cpp
+++ CLionTemplate/app/application.cpp
@@ -42,6 +42,7 @@ void showSystemInfo() {
   Serial.printf("boot mode: %d\r\n", system_get_boot_mode());
   Serial.printf("adc: %d\r\n", system_get_vdd33());
   Serial.printf("wifi ssid: %s\r\n", WifiStation.getSSID().c_str());
+}
 
 void blink() {
   digitalWrite(LED_PIN, (uint8_t) state);
