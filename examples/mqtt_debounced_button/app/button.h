92099654b2fd459ab6faca50b6542d27b9ec50b0
Bernhard Tittelbach <bernhard@tittelbach.org>
2017-06-07 22:06:22 +0200

    volatile_to_be_sure

--- examples/mqtt_debounced_button/app/button.h
+++ examples/mqtt_debounced_button/app/button.h
@@ -18,7 +18,7 @@ private:
 	int8_t button_pin_ = 0;
 	bool button_pulls_to_ground_ = true;
 
-	uint32_t button_event_ctr_ = 0;
+	volatile uint32_t button_event_ctr_ = 0;
 	uint32_t last_read_button_event_ctr_ = 0;
 	uint32_t last_button_event_time_ = 0;
 	uint32_t button_debounce_duration_ = 200;
